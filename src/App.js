c4b878a7a6ecb388babd44a57624b29e968758d6
Abhi Sree Kondu <konduabhisree@gmail.com>
2025-06-09 14:29:45 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Education from './components/Education';
-import Footer from './components/Footer';
+import Contact from './components/Contact';
 import "./App.css"
 function App() {
   return (
@@ -12,10 +12,10 @@ function App() {
       <Header />
       <div id="hero"><Hero /></div>
          <div id="education"><Education /></div>
-      <div id="about"><About /></div>
+<div id="skills"><Skills /></div>
       <div id="projects"><Projects /></div>
-      <div id="skills"><Skills /></div>
-      <div id="contact"><Footer /></div>
+      
+      <div id="contact"><Contact /></div>
    
     </div>
   );
