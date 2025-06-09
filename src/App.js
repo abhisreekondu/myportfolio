0424d6a16ba85cf48dfaf4f490e33ac18337fcac
Abhi Sree Kondu <konduabhisree@gmail.com>
2025-06-09 12:56:43 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-
-import './App.css';
-
-import Home from './components/Home';
+import Header from './components/Header';
+import Hero from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import Education from './components/Education';
 import Footer from './components/Footer';
-
+import "./App.css"
 function App() {
   return (
     <div className="bg-black text-white font-sans">
-      <Home />
-      <About />
-      <Projects />
-      <Skills />
-      <Footer />
+      <Header />
+      <div id="hero"><Hero /></div>
+         <div id="education"><Education /></div>
+      <div id="about"><About /></div>
+      <div id="projects"><Projects /></div>
+      <div id="skills"><Skills /></div>
+      <div id="contact"><Footer /></div>
+   
     </div>
   );
 }
 
 export default App;
-
